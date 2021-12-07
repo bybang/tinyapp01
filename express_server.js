af7888c23e8ab11ed4122a496e2f1192030d18a5
bybang <bybang26@gmail.com>
2021-12-07 14:47:36 -0500

    Added a partials and urls_show page

--- express_server.js
+++ express_server.js
@@ -29,6 +29,11 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  console.log(templateVars);
+  res.render("urls_show", templateVars);
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });

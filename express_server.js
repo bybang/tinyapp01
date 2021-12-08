11c0ae07431129e6847ec401f927a22f8c4ae572
bybang <bybang26@gmail.com>
2021-12-08 17:00:55 -0500

    Added cnew branch and cookie parser for cookies

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const generateRandomString = () => Math.random().toString(36).slice(7);
 
 // Database
